65133d0538be18048570e0e33f03d8a58f1e4b32
Christian <chrisadrianotav@gmail.com>
2020-04-07 20:04:59 -0400

    update try except finally todo

--- todos/week4/3_try_except_finallytodo.py
+++ todos/week4/3_try_except_finallytodo.py
@@ -2,6 +2,14 @@
 Modify the example from week 3 lesson 3's game function
 """
 
+# This example is from a previous lesson. We want it to ask a user how many times they want
+# to play the game, play the game that many times, and tell them when they guess correctly
+# along the way. One thing though- the example from the lessons doesn't account for if a user
+# inputs something thats NaN (not a number). Implement a try-except-finally to address if the user
+# gets a ValueError from entering a non-number. This should probably happen in the inquireUserGames
+# function as well as the playTheGame function to ensure that any time a user input is made,
+# your function can account for non-number entries.
+
 # Original Lesson Solution:
 from random import randint
 
@@ -46,31 +54,6 @@ def initializeGame():
         # Every time we run the playTheGame function we want to decrement the userGameCount
         # by one until it hits zero, at which point the while loop will stop running.
         userGameCount -= 1
-
-# Lets call the initializeGame function to get things going!
-initializeGame()
-
-# Make sure to comment out the above code before writing your solution.
-
-# This example is from a previous lesson. We want it to ask a user how many times they want
-# to play the game, play the game that many times, and tell them when they guess correctly
-# along the way. One thing though- the example from the lessons doesn't account for if a user
-# inputs something thats NaN (not a number). Implement a try-except-finally to address if the user
-# gets a ValueError from entering a non-number. This should probably happen in the inquireUserGames
-# function as well as the playTheGame function to ensure that any time a user input is made,
-# your function can account for non-number entries.
-
-from random import randint
-
-def inquireUserGames():
-    """ask a user how many times they want to play"""
-
-def playTheGame():
-    """ask user for a number between 1 and 10, generate a random number between 1 and 10, then see
-    if those numbers match. If they do, congratulate the user! If not, tell 'em they guess wrong"""
-
-def initializeGame():
-    """Lets initialize our game and call the inquireUserGames function."""
 
 # Lets call the initializeGame function to get things going!
 initializeGame()
