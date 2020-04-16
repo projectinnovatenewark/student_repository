61c125cf3c1d44c5bed8aee8d3503137799947af
Garrett Palmeri <garrettpalmeri@Garretts-MacBook-Pro.local>
2020-04-15 20:05:30 -0400

    Matrix todo

---
+++ todos/week3/7_advanced_funcs.py
@@ -0,0 +1,21 @@
+"""Use your knowledge from the Advanced 
+Funcs lesson to complete these Todos"""
+
+# TODO: You went to the store today for some food shopping and want to use a 
+# TODO: funciton to add what you bought. You want to be able to reuse
+# TODO: this though so you aren't sure how many arguments to use. Use *args 
+# TODO: as a parameter in your shoppingList function along with store as your other parameter.
+# TODO: Have your funciton print:
+# TODO: "Today I went to Shoprite and bought eggs, Beyond sausage, cheese, english muffins."
+# TODO: Hint: We don't want the brackets from the list in the print statement!
+
+
+
+# TODO: The above list is great and all, but it's be great and all...
+# TODO: but we don't have the costs of everything! in our new function shoppingDict,
+# TODO: pass store and **bought_item as parameters and we'll want to print:
+# TODO: "Today I went to shoprite and spent $22 on the following:"
+# TODO: Then for every item after, print "eggs: $4" for example.
+# TODO: Using the same items, eggs are $4, Beyond_sausage is $10, cheese is $3 and 
+# TODO: english_muffins are $5
+
--- todos/week4/2_matrices_with_liststodo.py
+++ todos/week4/2_matrices_with_liststodo.py
@@ -1,1 +1,17 @@
-# FIXME: ADD IN A SIMPLE LESSON ABOUT MATRICES
+"""TODO relating to Week 4 Matrices Lesson"""
+
+
+# TODO: Define a function called 'multiplier' that can create a multiplication table
+# TODO: by whatever paramters you set it to (row and column) and in it
+# TODO: create a matrix of all 0's and set it to the variable 'mult_table'
+# TODO: Pass the parameters row and col.
+
+
+# TODO: Using for loops and list comprehension to fill each cell in your 
+# TODO: 'mult_table' matrix with a cell from a multiplication table. 
+# TODO: mult_table[1][1] would equate to 1, mult_table[6][6] would equate to 36 etc...
+
+# TODO: Define a helper funciton named 'printer' and use it to print your 
+# TODO: multiplication table in a readable way. Hint: Use what you've learned
+# TODO: from previous lessons for reurning function values.
+# TODO: Also, your function should take inputs for how many rows and columns you want.
