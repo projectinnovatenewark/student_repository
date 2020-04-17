264b7b412a3a657eabc57c82aa9cb31f1cc0ada2
Garrett Palmeri <garrettpalmeri@Garretts-MBP.home>
2020-04-17 17:15:34 -0400

    removed lists methods todo answers

--- todos/week2/2_lists_and_dicts_methods.py
+++ todos/week2/2_lists_and_dicts_methods.py
@@ -4,25 +4,17 @@ pro_sports = ["NBA", "MLB", "WNBA", "NFL"]
 
 # TODO: Remove MLB from your shopping list using the .remove method then print the list
 
-pro_sports.remove("MLB")
-print(pro_sports)
 
 # TODO: Then add NHL to your list, then print the third item last item using index position
 # TODO: Save each to vairables named third_item and last_item
 # TODO: Print in this format: "Third Item: NFL - Last Item NHL"
 
-pro_sports.append("NHL")
-third_item = pro_sports[2]
-last_item = pro_sports[-1]
-print("Third Item:", third_item, "- Last Item:", last_item)
 
 # TODO: Now in the original list, remove NFL 
 # TODO: from the list using the .pop method and print NFL
 
 pro_sports = ["NBA", "MLB", "WNBA", "NFL"]
 
-football = pro_sports.pop()
-print(football)
 
 # TODO: The below dictionary contans some countries with their capitals,
 # TODO: Add Bangkok, Thailand to the dictionary.
@@ -34,14 +26,9 @@ countries = {
     "United States": "Washington D.C."
 }
 
-countries["Thailand"] = "Bangkok"
-
-print(countries)
 
 # TODO: Let's say we want to see if England is in our dictionary. Use the .get
 # TODO: method to return England's value. If England isn't in the dictionary
 # TODO: have the value returned be "Not in Dict". Set it to the 
 # TODO: variable get_england and print it.
 
-get_england = countries.get("England", "Not in Dict")
-print(get_england)
