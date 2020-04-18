d097f778e87ce020ff60fe3ed2428d8d18b9d154
Christian <chrisadrianotav@gmail.com>
2020-04-18 19:27:11 -0400

    update imports todo

--- todos/week2/2_lists_and_dicts_methodstodo.py
+++ todos/week2/2_lists_and_dicts_methodstodo.py
@@ -11,10 +11,9 @@ sweet_foods = ["ice cream", "skittles", "broccoli", "strawberries", "oreos", "as
 # TODO: Remove the last item in the list using the .pop() method. Do not set it equal to a variable.
 
 # TODO: Use the .pop() method again on the list. This time, use it to remove the first item in the list.
-# TODO: Set it equal to a variable and print that variable.
+# TODO: Set it equal to a variable called `first_item`, then print that variable.
 
-# TODO: For the item you just popped and set equal to a variable called `first_item`, then use that
-# TODO: variable and append it to the list.
+# TODO: Append the above variable to the `sweet_foods` list.
 
 # TODO: Print the sweet_foods list. Your output should be: ['skittles', 'strawberries', 'oreos', 'ice cream']
 
@@ -30,3 +29,11 @@ my_car = {"make": "Tesla", "model": "Cybertruck", "year": "2022"}
 
 # TODO: Use the .pop() method to remove the "year" key/value pair from the my_car dict. Set it equal
 # TODO: to a variable called `year`. Print the `year` variable and print the `my_car` dictionary.
+
+# Combining concepts
+
+# TODO: Append a string "year" to the sweet_foods list. Set a variable equal to the last item in the newly
+# TODO: updated list ( i.e. year_item = sweet_foods[4] ). Then, since the new variable will be equal
+# TODO: to a string of "year", you can use that variable to find the value of the key/value pair in the
+# TODO: my_car dictionary. Print out the output of using this variable to access that value
+# TODO: ex: print(my_car[year_item])
--- todos/week2/3_importstodo.py
+++ todos/week2/3_importstodo.py
@@ -1,1 +1,22 @@
-# FIXME: PUT IN A LESSON ON IMPORTS
+"""
+Use compounding interest rates and initial investment to find your return on investment
+and calculate the taxes owed
+"""
+
+# TODO: Import the annual_interest and tax_rate constants
+from interest import ANNUAL_INTEREST, TAX_RATE
+
+# TODO: Ask a user how much they would like to invest with an input. Set it equal to a variable
+# TODO: named `investment`.
+
+# TODO: Calculate how much their investment would appreciate in one year by multiplying
+# TODO: the investment variable by the annual_interest. Set the appreciation amount
+# TODO: equal to a variable named `appreciation`.
+
+# TODO: Create a new variable named `new_value` by adding the `appreciation` variable to the
+# TODO: `investment` variable.
+
+# TODO: Calculate the taxes you have to pay by setting a variable of `taxes` equal to
+# TODO: the `appreciation` variable times the tax_rate.
+
+# TODO: Print to the user the following output: "Your investment of _____ is now worth _____ and you owe _____ in taxes."
