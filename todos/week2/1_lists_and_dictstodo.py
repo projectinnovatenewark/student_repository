59ccf8f57db7eaf1615d12589e66bf00d9f0b05a
Christian <chrisadrianotav@gmail.com>
2020-04-18 13:55:24 -0400

    lists and dicts methods todo

--- todos/week2/1_lists_and_dictstodo.py
+++ todos/week2/1_lists_and_dictstodo.py
@@ -7,10 +7,10 @@ Reference lesson(s): week_2/1_lists_and_dicts.py
 DAYS_OF_WEEK = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 # TODO: In the following dictionary, the keys are associated with days of the week while
-# TODO: values represent temps the first value of 1 represents sunday's temperature all
-# TODO: the way to 7 which represents Saturday
-TEMPERATURE_FORECAST = {1: 85, 2: 70, 3: 80, 4: 72, 5: 67, 6: 95, 7: 100}
+# TODO: values represent temps the first value of 0 represents Sunday's temperature all
+# TODO: the way to 6 which represents Saturday
+TEMPERATURE_FORECAST = {"0": 85, "1": 70, "2": 80, "3": 72, "4": 67, "5": 95, "6": 100}
 
 # TODO: Print out Wednesday by accessing it through the list days of week, then print out
 # TODO: Wednesday's temperature by accessing the forecast dictionary. Your output should
-# TODO: match `The temperature on Wednesday will be 72 degrees.`
+# TODO: read `The temperature on Wednesday will be 72 degrees.`
--- todos/week2/2_lists_and_dicts_methodstodo.py
+++ todos/week2/2_lists_and_dicts_methodstodo.py
@@ -1,1 +1,32 @@
-# FIXME: ADD IN A LESSON ABOUT METHODS ON LISTS AND DICTS
+"""
+Using methods on lists and dictionaries
+"""
+
+# Lets start with list methods
+
+sweet_foods = ["ice cream", "skittles", "broccoli", "strawberries", "oreos", "asparagus"]
+
+# TODO: Remove "broccoli" from the sweet_foods list using .remove()
+
+# TODO: Remove the last item in the list using the .pop() method. Do not set it equal to a variable.
+
+# TODO: Use the .pop() method again on the list. This time, use it to remove the first item in the list.
+# TODO: Set it equal to a variable and print that variable.
+
+# TODO: For the item you just popped and set equal to a variable called `first_item`, then use that
+# TODO: variable and append it to the list.
+
+# TODO: Print the sweet_foods list. Your output should be: ['skittles', 'strawberries', 'oreos', 'ice cream']
+
+# Now onto dictionary methods
+
+my_car = {"make": "Tesla", "model": "Cybertruck", "year": "2022"}
+
+# TODO: Use the .get() method on the my_car dictionary to search for the `car_type` key. Set it equal to a
+# TODO: variable called c_type. If that key doesn't exist, have the .get() method return "type doesn't exist".
+# TODO: Print that variable.
+
+# TODO: Lets add a key/value pair of "car_type": "Electric" to the above my_car dictionary
+
+# TODO: Use the .pop() method to remove the "year" key/value pair from the my_car dict. Set it equal
+# TODO: to a variable called `year`. Print the `year` variable and print the `my_car` dictionary.
