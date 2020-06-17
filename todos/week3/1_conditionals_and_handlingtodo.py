61abafe33c18db80ced1b2f3a972f2c7e2c812cf
Daniel A Kontoh-Boateng <kontohda@shu.edu>
2020-06-16 21:07:58 -0400

    todo updates up to 6/16

--- todos/week3/1_conditionals_and_handlingtodo.py
+++ todos/week3/1_conditionals_and_handlingtodo.py
@@ -5,6 +5,9 @@ Actions you can perform on lists and dictionaries
 unconfirmed_users = ['alice', 'brian', 'candace', 'alshon']
 confirmed_users = []
 
+##################################################################################################
+# Section 1.
+
 # TODO: "Verify" each user by removing them one-by-one and move "verified"
 # TODO: users into the confirmed users list by using a while loop. Remember,
 # TODO: while loops will run for a list as long as there are items in it.
@@ -12,15 +15,14 @@ confirmed_users = []
 while unconfirmed_users:
 
 ##################################################################################################
+# Section 2.
 
 # TODO: display all the confirmed users with a for loop
-
-##################################################################################################
-
 # TODO: lets use list comprehension (reference from week 2, lesson 2) and create a new
 # TODO: list with every name capitalized from `confirmed users`
 
 ##################################################################################################
+# Section 3.
 
 # TODO: instead of using the pop method, let's setup a while loop that uses the length of the new list
 # TODO: of confirmed users to iterate through the names. In each name, we will want to add "confirmed",
