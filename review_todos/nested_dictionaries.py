7e5cc6b04a4310f871a33efe33c45242159c9fa2
Christian <chrisadrianotav@gmail.com>
2020-06-24 21:31:31 -0400

    remove an answer

--- review_todos/nested_dictionaries.py
+++ review_todos/nested_dictionaries.py
@@ -18,8 +18,3 @@ youth_groups = {
 
 # TODO: Output the following formatted message for each youth group:
 # TODO: "name1, name2, and name3 are a part of the (organization here). They meet in (location here) to (purpose here)."
-
-for g in youth_groups:
-    members = youth_groups[g]["members"]
-    mems = f"{members[0]}, {members[1]}, and {members[2]}"
-    print(mems)
