bee192a5836f120d2ba80d950d1338210c51eb9c
castaldoj <jc2252@njit.edu>
2020-06-29 20:15:36 -0400

    additional spacing changes

--- todos/week1/3_string_manipulationtodo.py
+++ todos/week1/3_string_manipulationtodo.py
@@ -3,20 +3,19 @@ Personality questionnaire
 Referenced lesson(s): week_1/2_variables_and_types.py && week_1/3_string_manipulation.py
 """
 
-# Section 1:
-
+# TODO: Section 1:
 # TODO: Ask a user to input their full name and set it equal to a variable of `full_name`.
 # TODO: Greet the user by printing the statement "Hello _____, welcome to your personality quiz!"
 
 # TODO: #########################################################################################
 
-# Section 2:
+# TODO: Section 2:
 # TODO: Use a function from our last lesson to capitalize the user's name within the above print
 # TODO: statement. Test it's functionality by entering a name with lowercase letters.
 
 # TODO: #########################################################################################
 
-# Section 3:
+# TODO: Section 3:
 # TODO: Write a program that asks a user 6 unique questions about them. After receiving all of the
 # TODO: inputs, make a series of print statements that reflect the question. For example, if you ask
 # TODO: someone their favorite movie, the output should read "Your favorite movie is _____".
