373da2c658bcceca56ce841451a71b3d6decd2be
Francine <francine.t.karpeh@gmail.com>
2020-06-29 20:23:37 -0400

    making changes for indexing and ranges todo

--- todos/week2/5_indexing_and_rangestodo.py
+++ todos/week2/5_indexing_and_rangestodo.py
@@ -2,17 +2,19 @@
 Working with for loops in dictionaries and lists
 """
 
-# Section 1 of TODO: #########################################################################################
-# Section 1 of TODO: Print numbers 5 through 10. Print each number using a basic for loop.
+# TODO: Section 1:
+# TODO: Print numbers 5 through 10. Print each number using a basic for loop.
 for num in range(5,11):
     print(num)
 print("\n")
 
-# Section 2 of TODO: #########################################################################################
-# Section 2 of TODO: Loop through the list above and "curve" each grade by 3 points. Print those
-# Section 2 of TODO: curved grades. In that loop, print out the student's number using the .index() function.
-# Section 2 of TODO: Your first iteration should read in the format "Student 1 got a 63." and that should
-# Section 2 of TODO: continue through all items in the list.
+# TODO:#############################################################################################
+
+# TODO: Section 2:
+# TODO: Loop through the list above and "curve" each grade by 3 points. Print those
+# TODO: curved grades. In that loop, print out the student's number using the .index() function.
+# TODO: Your first iteration should read in the format "Student 1 got a 63." and that should
+# TODO: continue through all items in the list.
 
 grades = [60, 73, 80, 87]
 
@@ -21,10 +23,11 @@ for grade in grades:
     print(cg)
 print("\n")
 
-# Section 3 of TODO: #########################################################################################
-# Section 3 of TODO:
-# Section 3 of TODO: Write a while loop and iterate through the list of todos. Use the "pop" method so that,
-# Section 3 of TODO: for each iteration, you can print the todo that you are removing from the todolist.
+# TODO:#############################################################################################
+
+# TODO: Section 3:
+# TODO: Write a while loop and iterate through the list of todos. Use the "pop" method so that,
+# TODO: for each iteration, you can print the todo that you are removing from the todolist.
 
 todos = ["exercise for fun", "eat food", "go to school", "write some code"]
 removed_todo = []
@@ -34,11 +37,11 @@ while todos:
     print("I don't want to " + current_todo, "anymore!")
     print("\n")
 
+# TODO:#############################################################################################
 
-# Section 4 of TODO: #########################################################################################
-# Section 4 of TODO:
-# Section 4 of TODO: Decrement var by 1 using a while loop. Print the number each time
-# Section 4 of TODO: it is decremented. Once the number reaches two, end the while loop.
+# TODO: Section 4:
+# TODO: Decrement var by 1 using a while loop. Print the number each time
+# TODO: it is decremented. Once the number reaches two, end the while loop.
 
 var = 7
 while var > 1:
@@ -46,15 +49,16 @@ while var > 1:
     print(var)
 print("\n")
 
-# Section 5 of TODO: #########################################################################################
-# Section 5 of TODO: 
-# Section 5 of TODO: in the following dictionary, keys are associated with days of the week & values represent
-# Section 5 of TODO: temps notice how, when in one line, the characters surpass our self-imposed 100 character
-# Section 5 of TODO: limit therefore, we can format our dictionary like such to stick to our format!
+# TODO:#############################################################################################
 
-# Section 5 of TODO: Here is a weather forecast with average temperatures for each day.
-# Section 5 of TODO: since our dictionary has a lot of items, let's split it over many lines.
-# Section 5 of TODO: Create a for loop that prints out each day of the week with the average temperature
+# TODO: Section 5: 
+# TODO: in the following dictionary, keys are associated with days of the week & values represent
+# TODO: temps notice how, when in one line, the characters surpass our self-imposed 100 character
+# TODO: limit therefore, we can format our dictionary like such to stick to our format!
+
+# TODO: Here is a weather forecast with average temperatures for each day.
+# TODO: since our dictionary has a lot of items, let's split it over many lines.
+# TODO: Create a for loop that prints out each day of the week with the average temperature
 
 TEMPERATURE_FORECAST = {
     "Sunday": 65,
