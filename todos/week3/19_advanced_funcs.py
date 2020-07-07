c65ffc6770f9beaa0d20623aa05b51f21931146c
castaldoj <jc2252@njit.edu>
2020-07-07 19:31:52 -0400

    there were no todos so addedd two to lesson 19

---
+++ todos/week3/19_advanced_funcs.py
@@ -0,0 +1,17 @@
+"""
+Using advanced functions
+"""
+
+# Section 1. Using *args
+# TODO: Create a function called add() that uses *num as a parameter.
+# TODO: Inside the function, create a loop which produces the sum of
+# TODO: a passed argument of any length and prints the result. 
+
+####################################################################################################
+
+# Section 2. Using **kwargs
+# TODO: Create a function concat() that uses **dicts as a parameter. 
+# TODO: Inside the function, create a loop that will iterate through 
+# TODO: a passed dictionary of any length and concatenate all of 
+# TODO: the values it finds.
+
