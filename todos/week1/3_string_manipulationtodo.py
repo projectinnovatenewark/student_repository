adf5eda3149991dc8755507cde6d297b5b43d936
markskurat <markskurat136@gmail.com>
2020-07-07 18:48:04 -0400

    Lesson 10 formatting

--- todos/week1/3_string_manipulationtodo.py
+++ todos/week1/3_string_manipulationtodo.py
@@ -2,15 +2,18 @@
 Personality questionnaire
 Referenced lesson(s): week_1/2_variables_and_types.py && week_1/3_string_manipulation.py
 """
-
+# TODO: Section 1:
 # TODO: Ask a user to input their full name and set it equal to a variable of `full_name`.
 # TODO: Greet the user by printing the statement "Hello _____, welcome to your personality quiz!"
 # TODO: Use a function from our last lesson to capitalize the user's name within the above print
 # TODO: statement. Test it's functionality by entering a name with lowercase letters.
 
-# TODO: Write a program that asks a user 6 unique questions about them. After receiving all of the
-# TODO: inputs, make a series of print statements that reflect the question. For example, if you ask
-# TODO: someone their favorite movie, the output should read "Your favorite movie is _____".
+# TODO: #########################################################################################
 
-# TODO: Print out the results in a neat format and make sure to capitalize the user's name with the
-# TODO: title function
+# TODO: Section 2:
+# TODO: Write a program that asks a user 6 unique questions about them. After receiving all of the
+# TODO: inputs, make a print statement in paragraph style that utilizes the .format function. Be 
+# TODO: sure to take advantage of the .title function in the print statement for names and words
+# TODO: that need to be capitalized! 
+
+# TODO: #########################################################################################
--- todos/week2/10_functions_and_scopetodo.py
+++ todos/week2/10_functions_and_scopetodo.py
@@ -1,7 +1,7 @@
 """
 Working with functions and scope
 """
-
+# TODO: Section 1:
 # TODO: create a function that takes a dictionary as an argument, loops through the
 # TODO: dictionary, then returns the iteration number, key, and value in a readable way
 # TODO: (i.e. "iteration number 1 returns the key firstKey and the value firstValue")
@@ -10,3 +10,4 @@ Working with functions and scope
 EXAMPLE_DICTIONARY = {"key1": "value1", "key2": "value2", "key3": "value3", "key4": "value4"}
 
 # TODO: place the function below
+
