8368c43ffd0513728dc7ee457e7f7583cd17b6f8
markskurat <markskurat136@gmail.com>
2020-07-07 19:16:15 -0400

    updated formatting for lesson 14

--- todos/week1/3_string_manipulationtodo.py
+++ todos/week1/3_string_manipulationtodo.py
@@ -2,18 +2,34 @@
 Personality questionnaire
 Referenced lesson(s): week_1/2_variables_and_types.py && week_1/3_string_manipulation.py
 """
+<<<<<<< HEAD
+=======
+
+>>>>>>> 9b57b22115242fbe326dbe088833dff3b4c0bcf9
 # TODO: Section 1:
 # TODO: Ask a user to input their full name and set it equal to a variable of `full_name`.
 # TODO: Greet the user by printing the statement "Hello _____, welcome to your personality quiz!"
+
+# TODO: #########################################################################################
+
+# TODO: Section 2:
 # TODO: Use a function from our last lesson to capitalize the user's name within the above print
 # TODO: statement. Test it's functionality by entering a name with lowercase letters.
 
 # TODO: #########################################################################################
 
+<<<<<<< HEAD
 # TODO: Section 2:
+=======
+# TODO: Section 3:
+>>>>>>> 9b57b22115242fbe326dbe088833dff3b4c0bcf9
 # TODO: Write a program that asks a user 6 unique questions about them. After receiving all of the
 # TODO: inputs, make a print statement in paragraph style that utilizes the .format function. Be 
 # TODO: sure to take advantage of the .title function in the print statement for names and words
 # TODO: that need to be capitalized! 
 
+<<<<<<< HEAD
 # TODO: #########################################################################################
+=======
+# TODO: Print out the results in a neat and readable format.
+>>>>>>> 9b57b22115242fbe326dbe088833dff3b4c0bcf9
