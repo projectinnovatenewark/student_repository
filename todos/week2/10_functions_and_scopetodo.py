7241e974e70815068144bc6d18ead89a7f021ffb
markskurat <markskurat136@gmail.com>
2020-07-14 17:47:57 -0400

    resolved comment lesson10 7.14.20 v2

--- todos/week2/10_functions_and_scopetodo.py
+++ todos/week2/10_functions_and_scopetodo.py
@@ -4,7 +4,7 @@ Working with functions and scope
 
 # TODO: Section 1:
 # TODO: Create a function that takes a dictionary as an argument, loops through the
-# TODO: dictionary, then returns the iteration number, key, and value in a readable way
+# TODO: dictionary, then returns the iteration number, key, and value in a readable way.
 # TODO: (i.e. "iteration number 1 returns the key firstKey and the value firstValue")
 
 # Here is an example dictionary:
