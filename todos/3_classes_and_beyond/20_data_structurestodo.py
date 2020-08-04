69db08f2a8f0c6b0ff5ebed96c67c7ec1d78e255
palmerigarrett <palmerigarrett@gmail.com>
2020-08-04 19:18:39 -0400

    Updated questions

--- todos/3_classes_and_beyond/20_data_structurestodo.py
+++ todos/3_classes_and_beyond/20_data_structurestodo.py
@@ -1,10 +1,22 @@
 """
 Create two classes for stacks and queues and include functions
-to define their methods
+to define their methods.
 """
 
-# TODO: define a class called stack and incude functions in the class
-# TODO: for push, pop, size (return length of list) and isEmpty which should reset the list to an empty list
+# TODO: Define a class called Stack and incude functions in the class
+# TODO: for push, pop, size (return length of list) and isEmpty which should check if the
+# TODO: list is empty. Create an instance of a stack using the list 
+# TODO: ['mess', 'up', 'some', 'commas']. To check if your functions are working correctly,
+# TODO: execute them in the following order: isEmpty(), push('yeah'), pop(), and lastly size().
 
-# TODO: define a class called queue and incude functions in the class
-# TODO: for queue, enqueue, dequeue, size (return length of list) and isEmpty which should reset the list to an empty list
+# Let's put a space between our stack and queue examples.
+print('\n')
+print('Lets transition from the stack to the queue example')
+print('\n')
+
+# TODO: Define a class called Queue and incude functions in the class
+# TODO: for queue, enqueue, dequeue, size (return length of list) and isEmpty which 
+# TODO: should check if the list is empty. Create an instance of a queue using the list 
+# TODO: ['hello', 'darkness', 'my', 'old']. To check if your functions are working correctly,
+# TODO: execute them in the following order: isEmpty(), enqueue('friend'), dequeue(), 
+# TODO: and lastly size().
