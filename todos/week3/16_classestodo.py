41005ede12dcedf8406acacad73c792eb2c73e7d
markskurat <markskurat136@gmail.com>
2020-08-05 21:03:08 -0400

    Updated classes TODO

--- todos/week3/16_classestodo.py
+++ todos/week3/16_classestodo.py
@@ -6,28 +6,30 @@ Creating classes for your classmates
 # TODO: Create a class for an Employee, and include basic data
 # TODO: like hours worked, salary, first name, last name, age, and title.
 
-# TODO: Create a function inside the class and print out a formatted
-# TODO: set of strings explaining the details from the employee. Use the f shorthand
+# TODO: Create a function 'printData' inside the class and print out a formatted
+# TODO: set of strings explaining the details from the employee that you make up. Use the f shorthand
 # TODO: for formatting that was used in the previous example.
 
 # TODO: Then, call that function to print out those strings with a class
 # TODO: of Employee set equal to a variable employeeOne.
 
-# TODO:#############################################################################################
+# TODO: #############################################################################################
 
 # TODO: Section 2:
 # TODO: Set up a child class that derives from the Employee class and add the attribute 'h_benefits'.
 # TODO: Define a function 'intro' to print:
-# TODO: "Hi, my name is Garrett Palmeri. I am a 24 year old Financial Analyst. I work 35 hours per week and make $40000 per year.
+# TODO: "Hi, my name is Yao Ming. I am a 39 year old Dentist. I work 60 hours per week and make 
+# TODO: $100000 per year.
 # TODO: I do not have insurance."
 
-# TODO:#############################################################################################
+# TODO: #############################################################################################
 
 # TODO: Section 3:
-# TODO: Create a class named 'Animal' with the attributes no_of_legs, weight, height. 
+# TODO: Create a class named 'Animal' with the attributes no_of_legs, weight, and height. 
 
 # TODO: Then you will create 2 classes derived from 'Animal' named 'Mammal' and 'Fish'. 
-# TODO: Define the variable 'habitat' for 'Mammal' and 'Fish' to = 'Land' and 'Water' respectively,
+# TODO: Define the variable 'habitat' for 'Mammal' and 'Fish'. They should be 
+# TODO: set equal to 'Land' and 'Water respectively when instantiated
 # TODO: so that it will apply to all Mammals and Fish.
 # TODO: Also give 'Mammal' a new attribute - 'can_fly'. 
 
