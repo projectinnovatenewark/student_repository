3520698e94cfd38a8c1e299aa65b6cf8a15b6b4a
Christian <chrisadrianotav@gmail.com>
2020-08-12 20:30:08 -0400

    update lesson 2

--- todos/1_basics/2_variables_and_typestodo.py
+++ todos/1_basics/2_variables_and_typestodo.py
@@ -3,58 +3,58 @@ Enjoy your first ToDo! This correlates to Lesson 2.
 """
 
 # TODO: Section 1:
-# TODO: Set a variable 'phrase1' euqal to "Hi everyone! " and another variable 'phrase2'
-# TODO: equal to "My name is [name here]" (complete with your name). Then use what you learned so
-# TODO: far in the lesson to set these varibles to a new variable called "complete"
-# TODO: and print it.
+# Set a variable 'phrase1' euqal to "Hi everyone! " and another variable 'phrase2'
+# equal to "My name is [name here]" (complete with your name). Then use what you learned so
+# far in the lesson to set these varibles to a new variable called "complete"
+# and print it.
 
-# TODO: #########################################################################################
+####################################################################################################
 
 # TODO: Section 2:
-# TODO: Set two variable in the same line, "flt1" and "flt2", equal to 3.5 and 14.0 respectiviely.
-# TODO: Then print each variable separately to check the output.
+# Set two variable in the same line, "flt1" and "flt2", equal to 3.5 and 14.0, respectively.
+# Then print each variable to check the output.
 
-# TODO: #########################################################################################
+####################################################################################################
 
 # TODO: Section 3:
-# TODO: Set a variable called name equal to your name, then set a variable of age
-# TODO: equal to your age as an integer. Print a statement with an output of
-# TODO: `My name is _____ and I am ______ years old.` First, try doing this using
-# TODO: "+" to combine your variables and the strings, then print the result.
+# Set a variable called name equal to your name, then set a variable of age
+# equal to your age as an integer. Print a statement with an output of
+# `My name is _____ and I am ______ years old.` First, try doing this using
+# "+" to combine your variables and the strings, then print the result.
 
-# Lesson Learned:
+# Takeaway:
 # This should throw you a fun error, because concatenation can only be done with
 # strings, not strings and integers/floats. Commas, however, simply print
-# separate elements rather than attempting to concatenate strings. Comment out your 
+# separate elements rather than attempting to concatenate strings. Comment out your
 # above code now to prevent the error from occuring as we move on.
 
-# TODO: Let's use commas to print our desired output instead. Refer to the output
-# TODO: above of `My name is _____ and I am ______ years old.`
+# Let's use commas to print our desired output instead. Refer to the output
+# above of `My name is _____ and I am ______ years old.`
 
-# TODO: #########################################################################################
+####################################################################################################
 
 # TODO: Section 4:
-# TODO: Set a variable equal to each of the types we have learned so far. That includes
-# TODO: integers, floats, booleans, None, and strings. So, have one variable per each
-# TODO: of those types (i.e. exampleInt = 0, exampleBool = False, etc.).
-# TODO: To check the type of something in python, you use the type() function.
-# TODO: For example, type("Hello") would yield string, and type(6) would yield int.
-# TODO: So, create on print statement that would print the type of each variable you've set.
+# Set a variable equal to each of the types we have learned so far. That includes
+# integers, floats, booleans, None, and strings. So, have one variable per each
+# of those types (i.e. exampleInt = 0, exampleBool = False, etc.).
+# To check the type of something in python, you use the type() function.
+# For example, type("Hello") would yield string, and type(6) would yield int.
+# So, create on print statement that would print the type of each variable you've set.
 
-# Lesson Learned:
+# Takeaway:
 # Types are important in Python and you can check different elements' types using type().
 # The type() function is an example of a polymorphic function, meaning that the same
 # function name can be used for different types. BIG. WORDS. GET. YOU. JOBS. 💰
 
-# TODO: Lastly, we will introduce type conversion. There are functions you can use to
-# TODO: convert things in Python. The first type conversion funciton we will use is str(). 
-# TODO: Try the statement from Section 3 instructed of using plus signs to concatenate strings to 
-# TODO: print `My name is _____ and I am ______ years old.` using the name and age variables.
-# TODO: However, this time when you're concatenating the age variable, wrap it in the
-# TODO: str() function as such: str(age). The print statement will no longer throw an
-# TODO: error 🎯 since age was converted to a string.
+# Lastly, we will introduce type conversion. There are functions you can use to
+# convert things in Python. The first type conversion funciton we will use is str().
+# Try the statement from Section 3 instructed of using plus signs to concatenate strings to
+# print `My name is _____ and I am ______ years old.` using the name and age variables.
+# However, this time when you're concatenating the age variable, wrap it in the
+# str() function as such: str(age). The print statement will no longer throw an
+# error 🎯 since age was converted to a string.
 
-# Lesson Learned:
+# Takeaway:
 # We can change types in Python when appropriate, and we learned one way to do so with str()
 
-# Good job, guys! First todo down 💪🏾
+# Good job, everyone! First todo down 💪🏾
