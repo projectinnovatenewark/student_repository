0d1339e8f3b8c6717de7b32c26f50aa193d07b78
Christian <chrisadrianotav@gmail.com>
2020-08-13 19:40:54 -0400

    update lesson 3

--- todos/1_basics/3_string_manipulationtodo.py
+++ todos/1_basics/3_string_manipulationtodo.py
@@ -4,28 +4,33 @@ Referenced lesson(s): 2 & 3
 """
 
 # TODO: Section 1:
-# Ask a user to input their full name and set it equal to a variable of `full_name`.
+# Ask a user to input their full name and set it equal to a variable of "full_name".
 # Greet the user by printing the statement "Hello _____, welcome to your personality quiz!"
 
-###################################################################################################
+####################################################################################################
 
 # TODO: Section 2:
-# Set a variable 'classic' to equal "thE GREat GatSBy". Then use functions learned in this section
+# Set a variable "classic" to equal "thE GREat GatSBy". Then use functions learned in this section
 # to capitalize the first letter of each word, make all characters lowercase, and all characters
-# uppercase. Then print each seperately.
+# uppercase. Set each of those variations equal to a variable. Print each variable on a different
+# line.
 
-###################################################################################################
+####################################################################################################
 
 # TODO: Section 3:
-# Write a program that asks a user 6 unique questions about them.
-# They can be anything like your favorite movie or foods. 
+# Write a program that asks a user 3 unique questions about them.
+# The questions can be anything like your favorite movie, food, sport, etc.
 
-# TODO: Section 3.1: 
+# TODO: Section 3.1:
 # After receiving all of the inputs, make a series of print statements that reflect the question.
-# For example, if you ask someone their favorite movie, the output should read:
-# "Your favorite movie is _____".
+# You should use f shorthand when inserting the variables into the strings. Your output should read
+# like the following:
 
-###################################################################################################
+# "1) Your favorite movie is Avatar."
+# "2) Your favorite food is pasta."
+# "3) Your favorite sport is track."
+
+####################################################################################################
 
 # TODO: Section 4:
 # It looks like the below list was meant to be one long string. Use what you learned in this
