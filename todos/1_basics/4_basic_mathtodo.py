18bac93de411df1e570a92a29d5956adae011934
palmerigarrett <palmerigarrett@gmail.com>
2020-08-19 10:57:15 -0400

    Hints, tips, takeaways, and more specific section 2

--- todos/1_basics/4_basic_mathtodo.py
+++ todos/1_basics/4_basic_mathtodo.py
@@ -4,16 +4,19 @@ Referenced lesson(s): week_1/4_basic_math.py
 """
 
 # TODO: Section 1:
+# TIP: Use print statements to test your code along the way.
+
 # Set the following variables and constants:
 # Set a constant of `TAX_RATE` equal to .20 (we will use this as twenty percent).
 
 # Receive a user input for the question "What is your company's name?". Set
-# the input equal to a variable called `company`
+# the input equal to a variable called "company".
 
 # Receive a user input for the question "What was your revenue for this year?". Set
-# the input equal to a variable called `revenue`
+# the input equal to a variable called "revenue".
+# HINT: What varible type do you want "revene" to be?
 
-# Set a variable equal to `taxes_paid`. Calculate the taxes by multipling the rate (20%)
+# Set a variable equal to "taxes_paid". Calculate the taxes by multipling the rate (20%)
 # from your previous variable by the revenue input from the user.
 
 # Set a new variable called `profit`. Calculate the revenue minus the taxes paid and set it
@@ -34,9 +37,13 @@ Referenced lesson(s): week_1/4_basic_math.py
 
 # TODO: Section 2:
 num_list = [35, 4, 20, 100, 96]
-# Print the minimum, maximum, and sum of num_list. Then print the absolute value of the sum.
+# Set the minimum, maximum, and sum of num_list to the variables "min_num", "max_num", and
+# "sum_list" respectively. Then print each in their own print statement in the format
+# "The ___ of num_list is ___." using f shorthand.
 
 # TODO: Seciton 2.1:
-num_list2 = [-20, 15, 27, -11]
-# Print the absolute value of the sum of 'num_list2'
+num_list2 = [-20, 15, -27, -11]
+# Find the sum of num_list2 and store it in the variable "sum2". Then print the sum in the format,
+# "The sum is ___". Then using f shorthand, return the value of the basolute value of "sum2" and
+# print in the format, "The absolute value of sum2 is ___".
 
