65eca11663acd7b096992fd45f1fa075c76dea30
Christian <chrisadrianotav@gmail.com>
2020-08-20 21:24:18 -0400

    save l5 updates

--- todos/1_basics/5_lists_and_dictstodo.py
+++ todos/1_basics/5_lists_and_dictstodo.py
@@ -6,7 +6,7 @@ Working with lists and dictionaries to convey a weather forecast
 # Identifying index values
 
 GREEK_LETTERS = ["Alpha", "Beta", "Gamma"]
-# Save the item in the list that is equal to index value "0" as a variable, then print the variable. 
+# Save the item in the list that is equal to index value "0" as a variable, then print the variable.
 # Print the item in the list that is equal to index value "-1" using [] (bracket notation).
 
 # Add the word "Delta" to the list "GREEK_LETTERS".
@@ -19,22 +19,26 @@ GREEK_LETTERS = ["Alpha", "Beta", "Gamma"]
 # Combining concepts of lists and dictionaries
 DAYS_OF_WEEK = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-# In the following dictionary, the keys are associated with days of the week and the
-# values represent temperatures. Dictionaries have indexes just like lists do.
+# In the following dictionary, the keys are associated with days of the week (i.e. Sunday = 0) and
+# the values represent temperatures. Dictionaries have indexes just like lists do.
 TEMPERATURE_FORECAST = {"0": 85, "1": 70, "2": 80, "3": 72, "4": 67, "5": 95, "6": 100}
 
 # Print out "Wednesday" with it's index value from the list "DAYS_OF_WEEK". Set that value equal
-# to a variable called "weds" which you will use in the print statement. Then, print
-# Wednesday's temperature by using the forecast dictionary. You want to use the "weds" variable
-# in your final print statement, but for the temperature, use it's value directly in the print
-# statement instead of setting a variable. Your output should read "The temperature on Wednesday
-# will be 72 degrees."
+# to a variable called "weds".
+
+# Print Wednesday's temperature by using the forecast dictionary.
+
+# Use the variable "weds" directly in the print statement below, but for the temperature
+# access the value directly in the print statement.
+# Your output should read "The temperature on Wednesday will be 72 degrees." Be sure to use
+# f shorthand! :D
 
 ####################################################################################################
 
 # TODO: Section 3:
-# Print out the first student of the first class followed by the
-# starting class time.
+# Set the first student of the first class to a variable.
+# Set the first class' starting class time to a variable.
+# Print each in separate print statements.
 classes = [
     {
         'subject': 'math',
