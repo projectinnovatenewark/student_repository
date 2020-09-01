2e722aba096505a1ffa3630da91b1cdc8709bac6
palmerigarrett <palmerigarrett@gmail.com>
2020-09-01 19:12:33 -0400

    Revamped todo_9

--- todos/1_basics/9_indexing_and_rangestodo.py
+++ todos/1_basics/9_indexing_and_rangestodo.py
@@ -5,45 +5,39 @@ Working with for loops in dictionaries and lists
 # TODO: Section 1:
 # Print numbers 5 through 10. Print each number using a basic for loop.
 
-##############################################################################################
+####################################################################################################
 
 # TODO: Section 2:
 # Loop through the list above and "curve" each grade by 3 points. Print those
-# curved grades. In that loop, print out the student's number using the .index() function.
-# Your first iteration should read in the format "Student 1 got a 63." and that should
+# curved grades. In that loop, print out the phone name using the .index() function.
+# Ypur print statement should read in the format "Phone [index + 1] is a [phone]." and that should
 # continue through all items in the list.
+phones = ["iPhone", "Galaxy", "Pixel", "Note"]
 
-grades = [60, 73, 80, 87]
+# TODO: Section 2.1:
+# Now loop through the dictionary below. This time use a variable to reference each key's value,
+# and print  "[company] is the maker of the [phone]."
+phones = {"Apple": "iPhone", "Samsung": "Galaxy", "Google": "Pixel", "Samsung": "Note"}
 
-##############################################################################################
+####################################################################################################
 
 # TODO: Section 3:
 # Write a while loop and iterate through the list of todos. Use the "pop" method so that,
-# for each iteration, you can print the todo that you are removing from the todolist.
-
+# for each iteration, you can then append the todo to the "removed_todo" list. Then print
+# the new list.
 todos = ["exercise for fun", "eat food", "go to school", "write some code"]
 removed_todo = []
 
-##############################################################################################
+####################################################################################################
 
-# TODO: Section 4:
-# Decrement var by 1 using a while loop. Print the number each time
-# it is decremented. Once the number reaches two, end the while loop.
-
-var = 7
-
-##############################################################################################
-
-# TODO: Section 5: 
+# TODO: Section 4: 
 # In the following dictionary, keys are associated with days of the week & values represent
-# temperatures.
+# temperatures. Here is a weather forecast with average temperatures for each day. Since our
+# dictionary has a lot of items, let's split it over many lines. Create a for loop that prints
+# out each day of the week with its associated temperature.
 
 # TIP: Notice how when in one line, the characters surpass our self-imposed 100 character
 # TIP: limit therefore, we can format our dictionary like such to stick to our format!
-
-# Here is a weather forecast with average temperatures for each day.
-# Since our dictionary has a lot of items, let's split it over many lines.
-# Create a for loop that prints out each day of the week with its associated temperature.
 
 TEMPERATURE_FORECAST = {
     "Sunday": 65,
