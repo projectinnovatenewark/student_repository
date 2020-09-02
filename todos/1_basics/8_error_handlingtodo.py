2ea344bba17c1e9446073f0d60d4c5700352596d
palmerigarrett <palmerigarrett@gmail.com>
2020-09-01 21:20:16 -0400

    Todo 8 Update

--- todos/1_basics/8_error_handlingtodo.py
+++ todos/1_basics/8_error_handlingtodo.py
@@ -1,24 +1,28 @@
 """
-Lets clean up some errors
+Lets clean up some errors!
 """
 
 
 # TODO: Section 1:
 
-# Run the below code to find the error, then comment out the code
-# and rewrite it with a new denominator to fix the error. Then print the 
-# type of error you got from the original problem.
+# Run the below to find the error. Use what you learned about error handling to find a solution.
+num1 = 32
+num2 = num1 - 32
+number = 10 * (num1/num2)
 
-number = 10 * (1/0) # FIXME
+# HINT:
+# Make sure to comment out the broken code after you find it so your program can execute properly.
 
 # TODO: Section 1.1:
-# Again, run the code below to find what type of error you are encountering. Then
-# define 'spam' as a variable and print your error type.
 
-equation = 4 + spam*3 # FIXME
+# Again, run the code below to find what type of error you are encountering. Then create a solution
+# for it.
+spam = x * 4
+equation = 4 + Spam
 
 # TODO: Section 1.2:
-# Again, run the code below to find what type of error you are encountering.
-# Change the code so that our '2 + 2' addition works and print your original error type.
 
-four = '2' + 2 # FIXME
+# Finally, run the last block of code below to find what type of error you are encountering.
+# Change the code so that the mathematical operation works correctly.
+three = "3"
+five = three + 2
