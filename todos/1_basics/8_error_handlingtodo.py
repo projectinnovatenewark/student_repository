9b0b5c991a6658b3983cf1e7c0ae6b46e0916032
palmerigarrett <palmerigarrett@gmail.com>
2020-09-03 14:31:56 -0400

    Commented out lines and marked with fixmes

--- todos/1_basics/8_error_handlingtodo.py
+++ todos/1_basics/8_error_handlingtodo.py
@@ -2,13 +2,15 @@
 Lets clean up some errors!
 """
 
+# Uncomment any lines of code that end in a # FIXME
 
 # TODO: Section 1:
 
 # Run the below to find the error. Use what you learned about error handling to find a solution.
-num1 = 32
-num2 = num1 - 32
-number = 10 * (num1/num2)
+
+# num1 = 32 # FIXME
+# num2 = num1 - 32 # FIXME
+# number = 10 * (num1/num2) # FIXME
 
 # HINT:
 # Make sure to comment out the broken code after you find it so your program can execute properly.
@@ -17,12 +19,14 @@ number = 10 * (num1/num2)
 
 # Again, run the code below to find what type of error you are encountering. Then create a solution
 # for it.
-spam = x * 4
-equation = 4 + Spam
+
+# spam = x * 4 # FIXME
+# equation = 4 + Spam # FIXME
 
 # TODO: Section 1.2:
 
 # Finally, run the last block of code below to find what type of error you are encountering.
 # Change the code so that the mathematical operation works correctly.
-three = "3"
-five = three + 2
+
+# three = "3" # FIXME
+# five = three + 2 # FIXME
