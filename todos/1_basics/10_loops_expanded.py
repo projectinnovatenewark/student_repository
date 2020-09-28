ba1eae25d30fa44c77542252e70aba5e56177de5
Christian <chrisadrianotav@gmail.com>
2020-09-27 21:39:58 -0400

    split lesson 9 into two todos

---
+++ todos/1_basics/10_loops_expanded.py
@@ -0,0 +1,18 @@
+"""
+Expanding on loops
+"""
+
+# TODO: Section 1:
+# Write a while loop and iterate through the list of todos. Use the "pop" method so that,
+# for each iteration, you can print the todo that you are removing from the todolist.
+
+todos = ["exercise for fun", "eat food", "go to school", "write some code"]
+removed_todo = []
+
+##############################################################################################
+
+# TODO: Section 2:
+# Decrement var by 1 using a while loop. Print the number each time
+# it is decremented. Once the number reaches two, end the while loop.
+
+var = 7
