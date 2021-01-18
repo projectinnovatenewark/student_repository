5c028dedebdbfb79f8db9a3a411c64d492e4e3d4
palmerigarrett <palmerigarrett@gmail.com>
2021-01-17 20:11:32 -0500

    review updates

---
+++ sem_1_review_exercises/exercise_1.py
@@ -0,0 +1,52 @@
+""" Pre-Reqs: 8_vars_and_types, 9_basic_math, 10_imports """
+
+# FIXME: #1 Write import statement here
+
+# TODO: E-1
+
+# Set a variable to each data type we've learned using the following variable names: fruit,
+# day_born, velo, is_wet.
+
+# Your first variable should be a string and equal to your favorite fruit.
+# Your second variable should be an integer and equal to the day of the month you were born.
+# Your third variable should be a float and equal to the velocity of gravity: 9.8.
+# The last variable should be a boolean and set to True or False based on the following
+# question: Is water wet?
+
+# In their own print statements, print each variable on its own line.
+
+# TODO: E-2
+
+# Define a variable called first_name equal to your first name. Then print, "Hello my name
+# is [first_name]." using f shorthand.
+
+# Next, define another variable called last_name and print a statement similar to the one above,
+# but this time using both first_name and last_name.
+
+# Finally create a print statement that includes your first name, last name, and a variable 
+# from E-1. EX: "My name is Jon Smith and the velocity of gravity is 9.8 mph."
+
+# TODO: E-3
+
+# Write a python program to convert feet to inches and store the answer in a variable called inches.
+# HINT: There are 12 inches in 1 foot.
+
+num_feet = 4
+
+# TODO: E-4
+
+# To begin this section, import "pi" *from* the "math" package in FIXME: #1.
+
+# Write a python program to find the area of a circle with a radius (r) of 2 and store it in a
+# variable called "area". The equation for such is: πr². Print "The area of a circle with a radius
+# of [r] is equal to [area]."
+r = 2
+
+# TODO: E-5
+
+# The print statement below will throw you an error. Revise and uncomment the print statement so you
+# can run it without receiving an error. #HINT: Errors in Python are generally descriptive!
+
+three = '3'
+# print(f"3 Plus 10 is {three + 10}")
+
